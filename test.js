0659302744a4815406b9ea079a7c55599b557f2f
wangchao96 <wangchao96@xdf.cn>
2024-06-14 17:52:10 +0800

    新加new 和 bind

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * @Author: wangchao96
  * @Date: 2024-06-12 16:58:24
  * @LastEditors: wangchao96
- * @LastEditTime: 2024-06-12 17:32:50
+ * @LastEditTime: 2024-06-14 17:45:07
  * @email: wangchao96@xdf.cn
  * @description: 功能介绍： 测试文件
  */
@@ -22,11 +22,31 @@ const resultCall = testCall.w_call(testObj, 1, 2, 3, 4)
 console.log(resultCall, '测试call')
 
 
-function testCall(a, b, c, d) {
+function testApply(a, b, c, d) {
     return {
         sum: a + b + c + d,
         name: this.name
     }
 }
-const resultApply = testCall.w_apply(testObj, [1, 2, 3, 3])
+const resultApply = testApply.w_apply(testObj, [1, 2, 3, 3])
 console.log(resultApply, '测试apply')
+
+
+function testBind(...arg) {
+    return {
+        sum: arg.reduce((val, item) => val += item, 0),
+        name: this.name
+    }
+}
+const resultBind = testBind.w_bind(testObj, 1, 2, 3, 3)(10)
+console.log(resultBind, '测试bind')
+
+function testNew(...arg) {
+    const name = 'wangchao'
+    return {}
+}
+testNew.prototype.getName = function() {
+    return 'aaaaa'
+}
+const resultNew = w_new(testNew)
+console.dir(resultNew.__proto__, {depth: null})
--- theory/thisChange.js
+++ theory/thisChange.js
@@ -2,7 +2,7 @@
  * @Author: wangchao96
  * @Date: 2024-06-12 16:41:43
  * @LastEditors: wangchao96
- * @LastEditTime: 2024-06-12 17:32:01
+ * @LastEditTime: 2024-06-14 17:51:33
  * @email: wangchao96@xdf.cn
  * @description: 功能介绍： call apply bind  new 
  */
@@ -32,4 +32,43 @@ Function.prototype.w_apply = function(context, arg) {
     context = context || {}
     context.fn = this
     return context.fn(...arg)
+}
+
+/**
+ * bind
+ * 传参: 第一个是新的this指向，后面参数任意多个
+ * 改变this指向为第一个参数
+ * 不立即执行
+ * 返回新的方法，该方法可以做构造函数
+ */
+Function.prototype.w_bind = function(context, ...arg1) {
+    const that = this
+    const bound = function(...arg2) {
+        const arg = [...arg1, ...arg2]
+        if (this instanceof bound) {
+            const result = that.call(this, ...arg)
+            return (typeof result === 'object' && result !== null) ? result : this
+        } else {
+            console.log(...arg)
+            return that.call(context, ...arg)
+        }
+
+    }
+    bound.prototype = Object.create(this.prototype)
+    bound.prototype.constructor = bound
+    return bound
+}
+
+/**
+ * new
+ * 返回一个对象
+ * 改变this指向为该对象
+ * 对象的原型链上继承形参原型
+ * 立即执行
+ */
+function w_new(execution, ...arg) {
+    const obj = {}
+    obj.__proto__ = execution.prototype
+    const result = execution.call(obj, ...arg)
+    return (typeof result === 'object' && result !== null) ? result : obj
 }
